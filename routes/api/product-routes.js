ceffd874b5af363c647e6d8b799a5ee01c7510ec
anthony3738-miZJgd <anthony3738@gmail.com>
2022-09-10 17:29:37 -0700

    Implemented /:id route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -13,22 +13,24 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   };
-
-
-
-  // find all products
-  // be sure to include its associated Category and Tag data
-
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag}],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
+
+
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -60,7 +62,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -101,7 +103,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
 });
 
